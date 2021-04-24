08784486089db56b8276ad9accfecae0639a9a85
Aleksei Lapshin <alexeyfonlapshin@gmail.com>
2021-04-24 18:06:57 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# jira-ugly-panels-hider
+Extension for google chrome to hide ugly panels in jira-cloud
---
+++ panel_hider.js
@@ -0,0 +1,53 @@
+var rightPanel = null;
+var leftPanel = null;
+var button = null;
+
+function addRightPanelHideButton() {
+	button = document.createElement("Button");
+	button.innerHTML = "show panel";
+	button.style = "top:0;right:0;position:absolute;z-index: 9999; font-family: monospace"
+	button.onclick = function() {
+		rightPanel.hidden = !rightPanel.hidden;
+		button.innerHTML = rightPanel.hidden ? "show panel" : "hide panel";
+	};
+	document.body.appendChild(button);
+}
+
+function checkRightPanel() {
+	var rightPanelTmp = document.getElementById('jira-issue-header-actions')?.parentNode?.parentNode;
+	if (!!rightPanelTmp && rightPanelTmp !== rightPanel) {
+		rightPanel = rightPanelTmp;
+		rightPanel.hidden = true;
+		if (!button) {
+			addRightPanelHideButton();
+		} else if (button.hidden) {
+			button.hidden = false;
+		}
+	} else if (!rightPanelTmp && button && !button.hidden) {
+		button.hidden = true;
+	}
+}
+
+function checkLeftPanel() {
+	var leftPanelTmp = document.getElementById('jira-issue-header')?.parentNode?.parentNode;
+	if (!!leftPanelTmp && leftPanelTmp !== leftPanel) {
+		leftPanel = leftPanelTmp;
+		leftPanel.style.paddingLeft = "0px";
+	}
+}
+
+function observeChanges() {
+  'use strict';
+  var observer = new MutationObserver(function(mutations) {
+  console.log(document.readyState)
+	checkRightPanel();
+	checkLeftPanel();
+  });
+  observer.observe(document, {
+    childList: true,
+    subtree: true
+  });
+};
+
+observeChanges();
+
